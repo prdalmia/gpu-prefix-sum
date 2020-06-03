61a3f1c142969e79ef82d3b0e79bfa87b0246f37
prdalmia <pdalmia@wisc.edu>
2020-06-02 22:49:20 -0500

    message

--- scan_standalone/scan.h
+++ scan_standalone/scan.h
@@ -13,8 +13,8 @@ void sum_scan_naive(unsigned int* const d_out,
 	const unsigned int* const d_in,
 	const size_t numElems);
 
-void sum_scan_blelloch(unsigned int* const d_out,
-	 unsigned int* const d_in,
+void sum_scan_blelloch(unsigned int*  d_out,
+	 unsigned int*  d_in,
 	const size_t numElems);
 
 #endif
