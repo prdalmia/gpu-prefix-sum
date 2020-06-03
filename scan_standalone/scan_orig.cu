a971d2274292a8f6dc1f237758a6b1ca9e6b1abd
prdalmia <pdalmia@wisc.edu>
2020-06-03 16:50:07 -0500

    message

--- scan_standalone/scan_orig.cu
+++ scan_standalone/scan_orig.cu
@@ -293,8 +293,10 @@ void gpu_prescan(unsigned int* const d_out,
 		s_out[max_elems_per_block - 1 
 			+ CONFLICT_FREE_OFFSET(max_elems_per_block - 1)] = 0;
 			//printf("D block sums is %d and a is %d\n", d_block_sums[blockIdx.x], gridDim.x);
-			printf("s[finalout] is %d and a is %d\n", d_block_sums[blockIdx.x], max_elems_per_block - 1 
+			if(gridDim.x == 70){
+							printf("s[finalout] is %d and a is %d\n", d_block_sums[blockIdx.x], max_elems_per_block - 1 
 			+ CONFLICT_FREE_OFFSET(max_elems_per_block - 1));
+			}
 		}
 
 	// Downsweep step
