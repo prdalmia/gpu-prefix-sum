99ab7dc76cd24ce07b90442c871bdd4471a7d765
prdalmia <pdalmia@wisc.edu>
2020-06-03 14:39:24 -0500

    message

--- scan_standalone/scan.cu
+++ scan_standalone/scan.cu
@@ -356,6 +356,7 @@ void gpu_prescan(unsigned int* d_out,
 	d_block_sums_2[id] = d_block_sums[id];
 	//printf("block sum is  %d and a is %d\n",d_block_sums[id], a);
 	}
+	grid.sync();
 	__syncthreads();
 	__threadfence();
 	if(a == len){
