22d0fd51e645be82c30dd509bf81a64d2efb4602
prdalmia <pdalmia@wisc.edu>
2020-06-03 23:04:07 -0500

    message

--- scan_standalone/scan_with_ccg.cu
+++ scan_standalone/scan_with_ccg.cu
@@ -1,6 +1,7 @@
 #include "scan.h"
 #include <stdio.h>
-
+#include <cooperative_groups.h>
+namespace cg = cooperative_groups;
 
 #define MAX_BLOCK_SZ 64
 #define NUM_BANKS 32
@@ -12,219 +13,6 @@
 #else
 #define CONFLICT_FREE_OFFSET(n) ((n) >> LOG_NUM_BANKS)
 #endif
-
-
-inline __device__ void cudaBarrierAtomicSubSRB(unsigned int * globalBarr,
-    // numBarr represents the number of
-    // TBs going to the barrier
-    const unsigned int numBarr,
-    int backoff,
-    const bool isMasterThread,
-    bool * volatile sense,
-    bool * volatile global_sense)
-{
-__syncthreads();
-if (isMasterThread)
-{
-//printf("Inside global Barrier for blockID %d and sense is %d and global sense is %d\n", blockIdx.x, *sense, *global_sense);
-// atomicInc acts as a store release, need TF to enforce ordering
-__threadfence();
-// atomicInc effectively adds 1 to atomic for each TB that's part of the
-// global barrier.
-atomicInc(globalBarr, 0x7FFFFFFF);
-//printf("Global barr is %d\n", *globalBarr);
-}
-__syncthreads();
-
-while (*global_sense != *sense)
-{
-if (isMasterThread)
-{
- //printf("Global sense hili\n");
-/*
-For the tree barrier we expect only 1 TB from each SM to enter the
-global barrier.  Since we are assuming an equal amount of work for all
-SMs, we can use the # of TBs reaching the barrier for the compare value
-here.  Once the atomic's value == numBarr, then reset the value to 0 and
-proceed because all of the TBs have reached the global barrier.
-*/
-if (atomicCAS(globalBarr, numBarr, 0) == numBarr) {
-// atomicCAS acts as a load acquire, need TF to enforce ordering
-__threadfence();
-*global_sense = *sense;
-//  printf("Setting global sense = sense \n");
-}
-else { // increase backoff to avoid repeatedly hammering global barrier
-// (capped) exponential backoff
-backoff = (((backoff << 1) + 1) & (1024-1));
-}
-}
-__syncthreads();
-
-// do exponential backoff to reduce the number of times we pound the global
-// barrier
-if (*global_sense != *sense) {
-for (int i = 0; i < backoff; ++i) { ; }
-__syncthreads();
-}
-}
-}
-
-inline __device__ void cudaBarrierAtomicSRB(unsigned int * barrierBuffers,
- // numBarr represents the number of
- // TBs going to the barrier
- const unsigned int numBarr,
- const bool isMasterThread,
- bool * volatile sense,
- bool * volatile global_sense)
-{
-__shared__ int backoff;
-
-if (isMasterThread) {
-backoff = 1;
-}
-__syncthreads();
-
-cudaBarrierAtomicSubSRB(barrierBuffers, numBarr, backoff, isMasterThread, sense, global_sense);
-}
-
-inline __device__ void cudaBarrierAtomicSubLocalSRB(unsigned int * perSMBarr,
-         const unsigned int numTBs_thisSM,
-         const bool isMasterThread,
-         bool * sense,
-         const int smID,
-         unsigned int* last_block)
-
-{
-__syncthreads();
-__shared__ bool s;
-if (isMasterThread)
-{
-s = !(*sense);
-// atomicInc acts as a store release, need TF to enforce ordering locally
-__threadfence_block();
-/*
-atomicInc effectively adds 1 to atomic for each TB that's part of the
-barrier.  For the local barrier, this requires using the per-CU
-locations.
-*/
-atomicInc(perSMBarr, 0x7FFFFFFF);
-}
-__syncthreads();
-
-while (*sense != s)
-{
-if (isMasterThread)
-{
-/*
-Once all of the TBs on this SM have incremented the value at atomic,
-then the value (for the local barrier) should be equal to the # of TBs
-on this SM.  Once that is true, then we want to reset the atomic to 0
-and proceed because all of the TBs on this SM have reached the local
-barrier.
-*/
-if (atomicCAS(perSMBarr, numTBs_thisSM, 0) == numTBs_thisSM) {
-// atomicCAS acts as a load acquire, need TF to enforce ordering
-// locally
-__threadfence_block();
-*sense = s;
-*last_block = blockIdx.x;
-}
-}
-__syncthreads();
-}
-}
-
-//Implements PerSM sense reversing barrier
-inline __device__ void cudaBarrierAtomicLocalSRB(unsigned int * perSMBarrierBuffers,
-               unsigned int * last_block,
-               const unsigned int smID,
-               const unsigned int numTBs_thisSM,
-               const bool isMasterThread,
-               bool* sense)
-{
-// each SM has MAX_BLOCKS locations in barrierBuffers, so my SM's locations
-// start at barrierBuffers[smID*MAX_BLOCKS]
-cudaBarrierAtomicSubLocalSRB(perSMBarrierBuffers, numTBs_thisSM, isMasterThread, sense, smID, last_block);
-}
-
-/*
-Helper function for joining the barrier with the atomic tree barrier.
-*/
-__device__ void joinBarrier_helperSRB(bool * global_sense,
-bool * perSMsense,
-bool * done,
-unsigned int* global_count,
-unsigned int* local_count,
-unsigned int* last_block,
-const unsigned int numBlocksAtBarr,
-const int smID,
-const int perSM_blockID,
-const int numTBs_perSM,
-const bool isMasterThread) {                                 
-__syncthreads();
-if (numTBs_perSM > 1) {
-cudaBarrierAtomicLocalSRB(&local_count[smID], &last_block[smID], smID, numTBs_perSM, isMasterThread, &perSMsense[smID]);
-
-// only 1 TB per SM needs to do the global barrier since we synchronized
-// the TBs locally first
-if (blockIdx.x == last_block[smID]) {
-    if(isMasterThread && perSM_blockID == 0){    
-    }
-    __syncthreads();
-cudaBarrierAtomicSRB(global_count, numBlocksAtBarr, isMasterThread , &perSMsense[smID], global_sense);  
-//*done = 1;
-}
-else {
-if(isMasterThread){
-while (*global_sense != perSMsense[smID]   ){  
-__threadfence();
-}
-}
-__syncthreads();
-}    
-} else { // if only 1 TB on the SM, no need for the local barriers
-    if(isMasterThread){
-    perSMsense[smID] = ~perSMsense[smID];
-    }
-cudaBarrierAtomicSRB(global_count, numBlocksAtBarr, isMasterThread,  &perSMsense[smID], global_sense);
-}
-}
-
-
-__device__ void kernelAtomicTreeBarrierUniqSRB( bool * global_sense,
-bool * perSMsense,
-bool * done,
-unsigned int* global_count,
-unsigned int* local_count,
-unsigned int* last_block,
-const int NUM_SM)
-{
-
-// local variables
-// thread 0 is master thread
-const bool isMasterThread = ((threadIdx.x == 0) && (threadIdx.y == 0) &&
-(threadIdx.z == 0));
-// represents the number of TBs going to the barrier (max NUM_SM, gridDim.x if
-// fewer TBs than SMs).
-const unsigned int numBlocksAtBarr = ((gridDim.x < NUM_SM) ? gridDim.x :
-NUM_SM);
-const int smID = (blockIdx.x % numBlocksAtBarr); // mod by # SMs to get SM ID
-// all thread blocks on the same SM access unique locations because the
-// barrier can't ensure DRF between TBs
-const int perSM_blockID = (blockIdx.x / numBlocksAtBarr);
-// given the gridDim.x, we can figure out how many TBs are on our SM -- assume
-// all SMs have an identical number of TBs
-
-int numTBs_perSM = (int)ceil((float)gridDim.x / numBlocksAtBarr);
-
-
-joinBarrier_helperSRB(global_sense, perSMsense, done, global_count, local_count, last_block,
-numBlocksAtBarr, smID, perSM_blockID, numTBs_perSM,
-isMasterThread);
-
-}
-
 
 
 
@@ -436,14 +224,7 @@ void gpu_prescan(unsigned int* d_out,
 	unsigned int*  d_block_sums_dummy_2, 
 	const unsigned int len,
 	const unsigned int shmem_sz,
-	const unsigned int max_elems_per_block,
-	bool * global_sense,
-    bool * perSMsense,
-    bool * done,
-    unsigned int* global_count,
-    unsigned int* local_count,
-    unsigned int* last_block,
-    const int NUM_SM)
+	const unsigned int max_elems_per_block)
 {
 	// Allocated on invocation
 	extern __shared__ unsigned int s_out[];
@@ -577,7 +358,7 @@ void gpu_prescan(unsigned int* d_out,
 }
     __syncthreads();
 }
-     kernelAtomicTreeBarrierUniqSRB(global_sense, perSMsense, done, global_count, local_count, last_block, NUM_SM);
+	grid.sync();
 	//if(a==len && id < gridDim.x){
 	//d_block_sums_2[id] = d_block_sums[id];
 	//printf("block sum is  %d and a is %d\n",d_block_sums[id], a);
@@ -676,7 +457,7 @@ void sum_scan_blelloch(unsigned int* d_out,
 	checkCudaErrors(cudaMemset(d_block_sums_dummy, 0, sizeof(unsigned int) * grid_sz));
 	checkCudaErrors(cudaMalloc(&d_block_sums_dummy_2, sizeof(unsigned int) * grid_sz));
 	checkCudaErrors(cudaMemset(d_block_sums_dummy_2, 0, sizeof(unsigned int) * grid_sz));
-    
+
 	// Sum scan data allocated to each block
 	//gpu_sum_scan_blelloch<<<grid_sz, block_sz, sizeof(unsigned int) * max_elems_per_block >>>(d_out, d_in, d_block_sums, numElems);
 	void *kernelArgs[] = {
@@ -684,52 +465,21 @@ void sum_scan_blelloch(unsigned int* d_out,
 	};
 	int numBlocksPerSm;
 	cudaOccupancyMaxActiveBlocksPerMultiprocessor(&numBlocksPerSm, gpu_prescan, block_sz, 0);
-  //Barrier Stuff
-	
-    unsigned int* global_count;
-    unsigned int* local_count; 
-    unsigned int *last_block;
-    bool * global_sense;
-    bool* perSMsense;
-    bool * done;
-    int NUM_SM = 80;
-    cudaMallocManaged((void **)&global_sense,sizeof(bool));
-    cudaMallocManaged((void **)&done,sizeof(bool));
-    cudaMallocManaged((void **)&perSMsense,NUM_SM*sizeof(bool));
-    cudaMallocManaged((void **)&last_block,sizeof(unsigned int)*(NUM_SM));
-    cudaMallocManaged((void **)&local_count,  NUM_SM*sizeof(unsigned int));
-    cudaMallocManaged((void **)&global_count,sizeof(unsigned int));
-    
-    cudaMemset(global_sense, false, sizeof(bool));
-    cudaMemset(done, false, sizeof(bool));
-    cudaMemset(global_count, 0, sizeof(unsigned int));
 
-    for (int i = 0; i < NUM_SM; ++i) {
-       cudaMemset(&perSMsense[i], false, sizeof(bool));
-       cudaMemset(&local_count[i], 0, sizeof(unsigned int));
-       cudaMemset(&last_block[i], 0, sizeof(unsigned int));
-     }
 	std::cout << "The numBLocks per Sm is " <<  numBlocksPerSm << std::endl;
-		//cudaLaunchCooperativeKernel((void*)gpu_prescan, grid_sz, block_sz,  kernelArgs, sizeof(unsigned int) * shmem_sz, 0);
-	
+		cudaLaunchCooperativeKernel((void*)gpu_prescan, grid_sz, block_sz,  kernelArgs, sizeof(unsigned int) * shmem_sz, 0);
+	cudaDeviceSynchronize();
+	/*
 	gpu_prescan<<<grid_sz, block_sz, sizeof(unsigned int) * shmem_sz>>>(d_out, 
 																	d_in, 
 																	d_block_sums,
+																	d_block_sums_2,
 																	d_block_sums_dummy, 
 																	d_block_sums_dummy_2,
 																	numElems, 
 																	shmem_sz,
-																	max_elems_per_block,
-																	global_sense, 
-																	perSMsense, 
-																	done, 
-																	global_count, 
-																	local_count, 
-																	last_block, 
-																	NUM_SM
-																);
-
-     cudaDeviceSynchronize();
+																	max_elems_per_block);
+*/
 	// Sum scan total sums produced by each block
 	// Use basic implementation if number of total sums is <= 2 * block_sz
 	//  (This requires only one block to do the scan)
@@ -748,6 +498,5 @@ void sum_scan_blelloch(unsigned int* d_out,
     gpu_add_block_sums<<<max_elems, block_sz>>>(d_block_sums, d_block_sums, d_block_sums_dummy, grid_sz);
 	gpu_add_block_sums<<<grid_sz, block_sz>>>(d_out, d_out, d_block_sums, numElems);
 
-	checkCudaErrors(cudaFree(d_block_sums));
-
+	//checkCudaErrors(cudaFree(d_block_sums));
 }
