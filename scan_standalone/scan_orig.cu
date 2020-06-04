47efa4736cfd4650f8f9ed458d0bfa3a22f5645e
prdalmia <pdalmia@wisc.edu>
2020-06-03 20:14:48 -0500

    message

--- scan_standalone/scan_orig.cu
+++ scan_standalone/scan_orig.cu
@@ -320,12 +320,12 @@ void gpu_prescan(unsigned int* const d_out,
 	{
 		d_out[cpy_idx] = s_out[ai + CONFLICT_FREE_OFFSET(ai)];
 		if( gridDim.x == 35){
-			printf("d[out] is %d and index is %d \n", d_out[cpy_idx], cpy_idx );
+			//printf("d[out] is %d and index is %d \n", d_out[cpy_idx], cpy_idx );
 		}
 		if (cpy_idx + blockDim.x < len){
 					d_out[cpy_idx + blockDim.x] = s_out[bi + CONFLICT_FREE_OFFSET(bi)];
 			if( gridDim.x == 35){
-				printf("d[out] is %d and index is %d \n", d_out[cpy_idx + blockDim.x], cpy_idx + blockDim.x);
+			//	printf("d[out] is %d and index is %d \n", d_out[cpy_idx + blockDim.x], cpy_idx + blockDim.x);
 					}
 				}
 	}
@@ -413,9 +413,13 @@ void sum_scan_blelloch(unsigned int*  d_out,
 		sum_scan_blelloch(d_block_sums, d_in_block_sums, grid_sz);
 		checkCudaErrors(cudaFree(d_in_block_sums));
 	}
-	/*
+	int max_elems = grid_sz/max_elems_per_block;
 	unsigned int* h_block_sums = new unsigned int[grid_sz];
 	checkCudaErrors(cudaMemcpy(h_block_sums, d_block_sums, sizeof(unsigned int) * grid_sz, cudaMemcpyDeviceToHost));
+	unsigned int* h_block_sums_out = new unsigned int[grid_sz];
+	checkCudaErrors(cudaMemcpy(h_block_sums_out, d_out, sizeof(unsigned int) * 2240, cudaMemcpyDeviceToHost));
+	if(grid_sz == 35){
+	
 	std::cout << "Block sums: ";
 	for (int i = 0; i <2240; ++i)
 	{
@@ -424,12 +428,10 @@ void sum_scan_blelloch(unsigned int*  d_out,
 	std::cout << std::endl;
 	std::cout << "Block sums length: " << grid_sz << std::endl;
 	
-	*/
-	//delete[] h_block_sums;
-	unsigned int* h_block_sums_out = new unsigned int[grid_sz];
-	checkCudaErrors(cudaMemcpy(h_block_sums_out, d_out, sizeof(unsigned int) * 2240, cudaMemcpyDeviceToHost));
-	int max_elems = grid_sz/max_elems_per_block;
-		if(grid_sz == 35){
+	
+	
+	
+
 	
 	
 	std::cout << "out sums: ";
@@ -441,7 +443,7 @@ void sum_scan_blelloch(unsigned int*  d_out,
 	std::cout << "out sums length: " << grid_sz << std::endl;
 	
 	}
-	
+	delete[] h_block_sums;
 	delete[] h_block_sums_out;
 	// Add each block's total sum to its scan output
 	// in order to get the final, global scanned array
